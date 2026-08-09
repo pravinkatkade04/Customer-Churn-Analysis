63b1dfb5d33cd114dbf11a71411bddc610ef9259
Pravin katkade <pravinkatkade999@gmail.com>
2026-08-10 02:53:35 +0530

    Add Customer Churn Analysis

---
+++ Customer_RawData_Visualization.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("customer_churn.csv")
+
+df = df.drop_duplicates()
+#know about the Column name and Nulls
+df . info()
+
+print(" \n Total Number of Rows :- " , len(df) )
+
+print("Age Columns Total Null Values :- ")
+print(df["Age"].isnull().sum())
+print(df["Age"].unique)
+
+df["Age"] = np.where((df["Age"]>100 )|(df["Age"]<0) , np.nan , df["Age"])
+df["Age"] = df["Age"].fillna(df["Age"].mean())
+print(df["Age"].dtype)
+df["Age"] = df["Age"].astype(int)
+
+print("Age Columns Total Null Values :- ")
+print(df["Age"].isnull().sum())
+print(df["Age"].dtype)
+
+print("\n \n")
+
+
+
+#Tenure Column
+print(df["Tenure"].describe())
+df["Tenure"] = np.where((df["Tenure"]>75)|(df["Tenure"]<0), df["Tenure"].mode() , df["Tenure"])
+print(" \n  Again We try To Check :- ",df["Tenure"].describe() , "\n ")
+print("\n ",df["Tenure"].info())
+print("Here Done Tenure Col \n\n ")
+
+#Gender Column 
+print(df["Gender"].isnull().sum())
+print(df["Gender"].unique())
+df["Gender"] = np.where(df["Gender"]=="male", df["Gender"].replace("male" , "Male" ), df["Gender"])
+df["Gender"] = np.where(df["Gender"]=="FEMALE" , df["Gender"].replace("FEMALE" , "Female"), df["Gender"])
+df["Gender"] = np.where(df["Gender"]=="unknown" , df["Gender"].mode(), df["Gender"])
+
+print("\n\n")
+
+
+#Contract Colum
+print(df["Contract"].info() , "\n \n ",df["Contract"].describe())
+print(df["Contract"].unique())
+
+df["Contract"] = df["Contract"].str.lower()
+df["Contract"] = df["Contract"].str.capitalize()
+print("After Cleaning the Data :- " , df["Contract"].unique())
+print("\n \n")
+
+
+
+#MonthlyCharges Column
